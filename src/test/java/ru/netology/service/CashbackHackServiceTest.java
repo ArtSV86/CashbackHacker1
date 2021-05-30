463ec508c5ec6f756b8830517643e32ad15790ad
Artur Sadov <Art.S.V.86@yandex.ru>
2021-05-30 18:44:07 +0400

    Change files

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 
 
 public class CashbackHackServiceTest {
@@ -11,14 +12,14 @@ public class CashbackHackServiceTest {
     public void shouldReturnNull() {
         int actual = service.remain(1000);
         int expected = 0;
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
     public void shouldReturn100() {
         int actual = service.remain(900);
         int expected = 100;
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 }
 
